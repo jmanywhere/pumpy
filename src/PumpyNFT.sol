db34a3432cfb818d27030e3f52d31a00eda54f03
Semi Invader <=>
2023-09-07 11:29:23 -0600

    pumpy nft halfway done

--- src/PumpyNFT.sol
+++ src/PumpyNFT.sol
@@ -2,23 +2,90 @@
 
 pragma solidity 0.8.21;
 
-import {ERC721} from "openzeppelin/token/ERC721/ERC721.sol";
-import "openzeppelin/security/ReentrancyGuard.sol";
+import {ERC721, Strings} from "openzeppelin/token/ERC721/ERC721.sol";
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "openzeppelin/security/ReentrancyGuard.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+import "forge-std/console.sol";
 
-contract PumpyNFT is ERC721, ReentrancyGuard {
-    mapping(uint _tokenId => uint _ROI) public pumpRet;
+error PUMPY_NFT_SUPPLY_EXCEEDED();
+error PUMPY_NFT_URI_SET();
+error PUMPY_NFT_NOT_MINTED(uint tokenId);
+
+contract PumpyNFT is ERC721, ReentrancyGuard, Ownable {
+    using Strings for uint256;
+    mapping(uint _tokenId => uint _ROI) public tokenType;
+    mapping(uint tokenType => uint roiValue) public pumpRet;
+    string private _uri;
     uint constant MAX_SUPPLY = 10000;
+    uint[] private _idPrompts;
+    uint public nftPrice;
 
-    uint totalSupply;
+    uint public totalSupply;
+    IERC20 public pumpy;
 
-    constructor() ERC721("PumpyNFT", "PUMPY_NFT") {}
+    constructor(
+        uint[] memory ids,
+        address _pumpy
+    ) ERC721("PumpyNFT", "PUMPY_NFT") {
+        pumpy = IERC20(_pumpy);
+        _idPrompts = ids;
+        pumpRet[1] = 5;
+        pumpRet[2] = 10;
+        pumpRet[3] = 20;
+        pumpRet[4] = 30;
+        pumpRet[5] = 40;
+        pumpRet[6] = 50;
+    }
 
     function mint(uint amount) external nonReentrant {
+        if (totalSupply + amount > MAX_SUPPLY)
+            revert PUMPY_NFT_SUPPLY_EXCEEDED();
+
         for (uint i = 0; i < amount; i++) {
-            uint id = totalSupply;
+            uint id = totalSupply + 1;
             totalSupply = id;
             _safeMint(msg.sender, id);
-            pumpRet[id] = 50;
+            setVal(id);
+        }
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        uint promptType = tokenType[tokenId];
+        if (promptType == 0) revert PUMPY_NFT_NOT_MINTED(tokenId);
+        // Only have 6 different types of images based on the return value
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, promptType.toString()))
+                : "";
+    }
+
+    function setUri(string memory uri) external onlyOwner {
+        if (bytes(_uri).length != 0) revert PUMPY_NFT_URI_SET();
+        _uri = uri;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _uri;
+    }
+
+    function setVal(uint id) private {
+        if (_idPrompts.length == 0) return;
+
+        uint finalIndex = _idPrompts.length - 1;
+        uint finalVal = _idPrompts[finalIndex];
+
+        if (finalVal > 0) {
+            uint roiId = finalVal & 0x0f;
+            tokenType[id] = roiId;
+            finalVal = finalVal >> 4;
+            _idPrompts[finalIndex] = finalVal;
+            if (_idPrompts[finalIndex] == 0) {
+                _idPrompts.pop();
+            }
         }
     }
 }
---
+++ test/NFT.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import "forge-std/Test.sol";
+import {PumpyNFT} from "../src/PumpyNFT.sol";
+import {PUMPY} from "../src/Pumpy.sol";
+
+contract TestNFT is Test {
+    PumpyNFT nft;
+    PUMPY pumpy;
+
+    address user1 = makeAddr("user1");
+    address user2 = makeAddr("user2");
+
+    function setUp() public {
+        uint[] memory values = new uint[](11);
+        values[
+            0
+        ] = 0x3224451213233112661131114655134522253121224131222111342223226233;
+        values[
+            1
+        ] = 0x5233133312614634224242214523522111221511156312341132553125123413;
+        values[
+            2
+        ] = 0x1322221312222111111155222213212412111432221512441222111111311312;
+        values[
+            3
+        ] = 0x2361131511141313362221326211126223453261211242133211154514111632;
+        values[
+            4
+        ] = 0x1523241162532341223425222122221213161122443415432412251112222533;
+        values[
+            5
+        ] = 0x5243412115416441112623231412213143311642121316221222311423432162;
+        values[
+            6
+        ] = 0x4215111342412122111132344212365111163422222223251311141131243622;
+        values[
+            7
+        ] = 0x6254113244341121312321313122351125425223541212112245224522516161;
+        values[
+            8
+        ] = 0x1212521226145312142212212211111524322232234533443514136254133433;
+        values[
+            9
+        ] = 0x1212336222143213122312152212152233411112111122214421116411325225;
+        values[10] = 0x32;
+        pumpy = new PUMPY();
+        nft = new PumpyNFT(values, address(this));
+
+        pumpy.transfer(user1, 100 ether);
+    }
+
+    function test_mint() public {
+        vm.prank(user1);
+        nft.mint(2);
+
+        assertEq(nft.balanceOf(user1), 2);
+        assertEq(nft.totalSupply(), 2);
+        assertEq(nft.ownerOf(1), user1);
+        assertEq(nft.ownerOf(2), user1);
+        assertEq(nft.tokenType(1), 2);
+        assertEq(nft.tokenType(2), 3);
+
+        nft.setUri("test/");
+
+        assertEq(nft.tokenURI(1), "test/2");
+        assertEq(nft.tokenURI(2), "test/3");
+
+        vm.prank(user2);
+        nft.mint(1);
+
+        assertEq(nft.tokenURI(3), "test/5");
+        assertEq(nft.tokenType(3), 5);
+
+        vm.prank(user2);
+        nft.mint(1);
+
+        assertEq(nft.tokenURI(4), "test/2");
+        assertEq(nft.tokenType(4), 2);
+    }
+}
