c846ef67ed2df5853aa39e77854b18ba10669f2d
Semi Invader <=>
2023-09-08 16:37:36 -0600

    interace complete

--- src/interfaces/IPumpyStaking.sol
+++ src/interfaces/IPumpyStaking.sol
@@ -2,14 +2,75 @@
 pragma solidity 0.8.21;
 
 interface IPumpyStaking {
-    
-    function stake(uint256 nftId, uint256 amount) external;
-    
-    function claimRewards() external;
-    
-    function unstake() external;
-    
-    function stakedAmount(address user) external view returns (uint256);
-    
-    function stakingStartTime(address user) external view returns (uint256);
+    //-------------------------------------------------------------------------
+    // Type definitions
+    //-------------------------------------------------------------------------
+    struct StakingInfo {
+        uint256 depositAmount;
+        uint256 lastAction;
+        uint256 nftId;
+        uint256 nftRoi;
+    }
+
+    //-------------------------------------------------------------------------
+    // Events
+    //-------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------
+    // Interface Functions
+    //-------------------------------------------------------------------------
+    /**
+     * @notice Stakes PUMP tokens with an NFT to get daily ROI
+     * @param nftId id of the NFT to stake
+     * @param amount amount of PUMPY tokens to stake
+     * @dev The NFT must be owned by the caller
+     *      - Staking with another NFT ID is not possible
+     *      - Adding more PUMPY tokens to an existing stake is only when NFT id matches
+     *      - Adding more PUMPY compounds current rewards
+     */
+    function deposit(uint256 nftId, uint256 amount) external;
+
+    /**
+     * @notice Claims rewards for the caller and optionally compounds them
+     * @param claimAndCompound if true, rewards are claimed and added to the stake Deposit
+     */
+    function claimRewards(bool claimAndCompound) external;
+
+    /**
+     * @notice Unstakes PUMPY tokens and returns the NFT to the caller
+     * @dev Rewards are claimed
+     * @dev Reset user position after withdraw
+     */
+    function withdraw() external;
+
+    /**
+     * @notice Returns the staking info for a user
+     * @param user address of the user
+     * @dev this is a mapping of user address to StakingInfo struct
+     */
+    function userInfo(address user) external view returns (StakingInfo memory);
+
+    /**
+     * @notice Returns the total amount of PUMPY tokens received by all users
+     */
+    function totalRewardsGiven() external view returns (uint256);
+
+    /**
+     * @notice Returns the total amount of PUMPY tokens staked by all users
+     */
+    function totalStakes() external view returns (uint256);
+
+    /**
+     * @notice Returns the extimated end time of the staking pool rewards
+     * @dev with the help of an internal variable `rewardsPerSecond` we can calculate the end time
+     */
+    function estimatedEndTime() external view returns (uint256);
+
+    /**
+     * @notice Returns the total amount of PUMPY tokens in the staking pool that are designated to rewards
+     * @dev PUMPY.balanceOf(this) - totalStakes()
+     * -- INVARIANT --
+     * @dev balance should only be  >= totalStakes()
+     */
+    function rewardPool() external view returns (uint256);
 }
