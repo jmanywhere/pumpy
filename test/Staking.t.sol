e38b563fee93670319ebee4252dce44dea8bd81c
Gabriel Garoz <gab.120gg@gmail.com>
2023-09-11 19:31:06 -0600

    setup users for testing

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -11,12 +11,23 @@ contract StakingTest is Test {
     PumpyNFT public nft;
     PumpyStaking public staking;
 
+    address user1 = makeAddr("user1");
+    address user2 = makeAddr("user2");    
+
     function setUp() public {
         pumpy = new PUMPY();
         uint256[] memory ids = new uint256[](1);
         ids[0] = 0x112233;
         nft = new PumpyNFT(ids, address(pumpy));
         staking = new PumpyStaking(address(pumpy), address(nft));
+
+        nft.setNFTPrice(1 ether);
+        pumpy.transfer(user1, 100 ether);
+        pumpy.transfer(user2, 200 ether);
+        vm.prank(user1);
+        pumpy.approve(address(nft), 100 ether);
+        vm.prank(user2);
+        pumpy.approve(address(nft), 200 ether);        
     }
 
     function test_deposit() public {
@@ -27,5 +38,6 @@ contract StakingTest is Test {
         // value 0xf = binary 1111
         // 0x12 & 0xf = 0x02
         // 0x12 >> 4 = 00010010 >> 4 = 00000001 = 0x01
+
     }
 }
