a64471c66ff7a8626b9d76fef6674a48f5071824
Chuz <susguzman@gmail.com>
2023-09-12 18:06:58 +0000

    staking fixes

--- src/PumpyStaking.sol
+++ src/PumpyStaking.sol
@@ -12,16 +12,14 @@ contract PumpyStaking is IPumpyStaking {
     PumpyNFT public nft;
 
     // State variables
-    uint256 public totalStakes;
     uint256 public rewardPool;
+    uint256 public totalStakes;
     uint256 public totalRewardsGiven;
     // uint256 public lastUpdated; // Timestamp of the last time the reward pool was updated    
     
     mapping(address _user => StakingInfo) public userInfo;
-    mapping(address => uint256) public stakedPumpyAmount;
-    mapping(address => uint256) public stakingStartTime;
-    mapping(uint256 => uint256) public nftToROI;
-    mapping(address => uint256) public lastClaim;
+    //mapping(address => uint256) public stakedPumpyAmount;
+    //mapping(address => uint256) public lastClaim;
 
     constructor(address _pumpy, address _nft) {
         pumpy = PUMPY(_pumpy);
@@ -38,11 +36,14 @@ contract PumpyStaking is IPumpyStaking {
 
         userInfo[msg.sender].nftId = nftId;
         userInfo[msg.sender].lastAction = block.timestamp;
-        stakedPumpyAmount[msg.sender] += amount;
+        userInfo[msg.sender].depositAmount += amount;
         totalStakes += amount;
 
-        lastClaim[msg.sender] = block.timestamp;
+        // lastClaim[msg.sender] = block.timestamp; This is not needed
+
+        // @TODO
         // If user makes a deposit on the same NFT, claim rewards and compound them?
+
         emit Deposit(msg.sender, nftId, amount);
     }
 
@@ -50,49 +51,50 @@ contract PumpyStaking is IPumpyStaking {
 
         uint256 nftId = userInfo[msg.sender].nftId;
         uint256 pumpRet = nft.pumpRet(nft.tokenType(nftId));
-        uint256 timeElapsed = block.timestamp - lastClaim[msg.sender];
-        uint256 oneDay = 86400; // Number of seconds in a day
-        uint256 dailyRewards = (stakedPumpyAmount[msg.sender] * pumpRet) / 1000;
-        uint256 rewards = (dailyRewards * timeElapsed) / oneDay;
+        uint256 timeElapsed = block.timestamp - userInfo[msg.sender].lastAction;
+        uint256 dailyRewards = (userInfo[msg.sender].depositAmount * pumpRet) / 1000;
+        uint256 rewards = (dailyRewards * timeElapsed) / 86400/*oneDay*/ ;
 
         require(nftId != 0, "You are not staking any NFTs");
-        require(block.timestamp > lastClaim[msg.sender] + 1 days, "You can claim your rewards only once per day");
+        require(block.timestamp > userInfo[msg.sender].lastAction + 1 days, "You can claim your rewards only once per day");
         require(pumpy.balanceOf(address(this)) >= rewards, "There are not enough PUMPY tokens to pay rewards");
 
         if (isCompound == true) {
-            stakedPumpyAmount[msg.sender] += rewards;
+            userInfo[msg.sender].depositAmount += rewards;
             totalStakes += rewards;
         } else {
             pumpy.transfer(msg.sender, rewards);
             // Update state variables
+            userInfo[msg.sender].totalRewards += rewards;
             totalRewardsGiven += rewards;
         }
 
         rewardPool = totalStakes - rewards;
-        lastClaim[msg.sender] = block.timestamp;
+
+        userInfo[msg.sender].lastAction = block.timestamp;
 
         emit Claim(msg.sender, nftId, rewards);
     }
 
     function withdraw() external {
-        require(stakedPumpyAmount[msg.sender] > 0, "You are not staking any tokens");
+        require(userInfo[msg.sender].depositAmount > 0, "You are not staking any tokens");
 
         claimRewards(false);
 
-        uint256 amountToWithdraw = stakedPumpyAmount[msg.sender];
+        uint256 amountToWithdraw = userInfo[msg.sender].depositAmount;
         uint256 nftId = userInfo[msg.sender].nftId;
 
         require(pumpy.transfer(msg.sender, amountToWithdraw), "There are not enough PUMPY tokens to pay rewards");
         
         totalStakes -= amountToWithdraw;
-        stakedPumpyAmount[msg.sender] = 0;
+        userInfo[msg.sender].depositAmount = 0;
 
         nft.transferFrom(address(this), msg.sender, nftId); // return NFT to the owner
 
         emit Withdraw(msg.sender, nftId);
     }
  
-     function estimatedEndTime() external view returns (uint256) {
+    function estimatedEndTime() external pure returns (uint256) {
         // TODO: Implement function logic
         return 0;  // Placeholder return
     }
